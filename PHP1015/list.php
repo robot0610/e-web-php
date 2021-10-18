0c55ad9274c1bf14c164a1c2acd0eb5a933bc0e9
Peter Lin <m45lorenzi3832@gmail.com>
2021-10-18 10:56:21 +0800

    質數練習

--- PHP1015/list.php
+++ PHP1015/list.php
@@ -3,10 +3,28 @@
 3.5.7.11.13.17.......97
 
 <?php
-
+echo "<br>";
 for($i=3;$i<100;$i=$i*2+1)
 {
     echo $i.",";
 }
+
+echo "<br>";
+
+for($i=3;$i<100;$i++)
+    {
+    $test=false; // 表示質數
+   for($j=2;$j<$i;$j++)
+    if($i % $j == 0)
+    {
+        $test=true; //不是質數
+    }
+
+    if($test==false)
+    {
+        echo $i.",";
+    }
+}
+
 
 ?>
