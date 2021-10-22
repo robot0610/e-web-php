a5d12cccb49255ec4b69f46b3dbff3957cfec8ce
Peter Lin <m45lorenzi3832@gmail.com>
2021-10-22 16:07:40 +0800

    zodiac

--- array/array03.php
+++ array/array03.php
@@ -5,6 +5,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>威力彩</title>
+    <style>
+        .ball
+        {
+            width:30px;
+            height:30px;
+            background-color:lightgreen;
+            margin:5px 0px;
+            text-align:center;
+            padding:10px 0 0px 0px ;
+            border-radius:50%;
+        }
+    </style>
 </head>
 <body>
     <h1>威力彩選號不重複</h1>
@@ -28,7 +40,12 @@ print_r($nums);
 echo "</pre>";
 echo "迴圈次數".$i;
 
+echo "<br>";
 
+foreach($nums as $num)
+{
+    echo "<div class='ball';>".$num."</div>";
+}
 
 
 ?>
